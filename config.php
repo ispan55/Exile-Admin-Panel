59b5ca2d46b5cab9a31d31fd948b9c763797cee0
itsmadhatter <alexander.treichel@gmx.net>
2017-02-12 04:58:12 +0100

    Rework

--- config.php
+++ config.php
@@ -1,9 +1,14 @@
 <?php
 
 define('ROOT_PATH', __DIR__ . '/');
+define('UPLOAD_DIR', ROOT_PATH . 'uploads/files');
 
-define('HOME_URL', '192.168.178.51');
-define('HOME_DIR', '192.168.178.51/');
+define('HOME_URL', '10.0.0.184');
+define('HOME_DIR', '10.0.0.127/a3ap/');
+
+$rcon = null;
+$db_exile = null;
+$db_panel = null;
 
 
 /**
@@ -19,7 +24,7 @@ define('DB_PANEL_HOST', 'localhost');
 define('DB_PANEL_USER', 'root');
 
 /** PASSWORD */
-define('DB_PANEL_PASS', 'alex1998');
+define('DB_PANEL_PASS', '');
 
 /** DATABASE NAME */
 define('DB_PANEL_NAME', 'exileadmin');
@@ -38,7 +43,7 @@ define('DB_EXILE_HOST', 'localhost');
 define('DB_EXILE_USER', 'root');
 
 /** PASSWORD */
-define('DB_EXILE_PASS', 'alex1998');
+define('DB_EXILE_PASS', '');
 
 /** DATABASE NAME */
 define('DB_EXILE_NAME', 'exile');
@@ -59,34 +64,34 @@ define('RCON_PASS', 'alex1998');
 /** PASSWORD */
 define('RCON_PORT', 2306);
 
-
-/**
- * LOAD EXILE DATABASE
- *
- *
- */
-
-require_once('include/db/DatabaseExile.php');
-$db_exile = new DatabaseExile(DB_EXILE_HOST, DB_EXILE_USER, DB_EXILE_PASS, DB_EXILE_NAME);
-
-/**
- * LOAD PANEL DATABASE
- *
- *
- */
-
-require_once('include/db/DatabasePanel.php');
-$db_panel = new DatabasePanel(DB_PANEL_HOST, DB_PANEL_USER, DB_PANEL_PASS, DB_PANEL_NAME);
-
-/**
- * LOAD RCON
- *
- *
- */
-
-require_once('include/class/Rcon.php');
 try {
+    /**
+     * LOAD EXILE DATABASE
+     *
+     *
+     */
+
+    require_once('include/db/DatabaseExile.php');
+    $db_exile = new DatabaseExile(DB_EXILE_HOST, DB_EXILE_USER, DB_EXILE_PASS, DB_EXILE_NAME);
+
+    /**
+     * LOAD PANEL DATABASE
+     *
+     *
+     */
+
+    require_once('include/db/DatabasePanel.php');
+    $db_panel = new DatabasePanel(DB_PANEL_HOST, DB_PANEL_USER, DB_PANEL_PASS, DB_PANEL_NAME);
+
+    /**
+     * LOAD RCON
+     *
+     *
+     */
+
+    require_once('include/class/Rcon.php');
     $rcon = new Rcon(RCON_HOST, RCON_PASS, RCON_PORT);
-} catch (Exception $exception) {
-    print $exception;
+
+} catch (Exception $e) {
+    $_SESSION['error'] = 'Error: Cant Connect to Server!';
 }
